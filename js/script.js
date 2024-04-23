766ba36eeab74fd6aaaa77d9febe45c0f3e7220c
Daniel Turner <lunartechfreek@googlemail.com>
2024-04-23 18:07:49 +0000

    Add code to target buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $('p').click(function() {
     $(this).slideToggle('slow');
 });
+
+$('button').mouseenter(function() {
+    $(this).removeClass('makeRed').addClass('makeBlue');
+});
+
+$('button').mouseleave(function() {
+    $(this).removeClass('makeBlue').addClass('makeRed');
 });
